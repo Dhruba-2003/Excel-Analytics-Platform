73a5acf1b9c0fa4bed0d21d97d241358cb123968
Dhruba-2003 <ghoshdhrubajyoti25@gmail.com>
2025-08-29 12:51:31 +0530

    Fix import casing in userRoutes.js

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { registerUser, loginUser, getUserProfile, updateUserProfile } from '../controllers/userController.js';
+import { registerUser, loginUser, getUserProfile, updateUserProfile } from '../controllers/usercontroller.js';
 import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
