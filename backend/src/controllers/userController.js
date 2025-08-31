a223c5b6fbebfb147a40e6f72b348edf4a98b939
Dhruba-2003 <ghoshdhrubajyoti25@gmail.com>
2025-08-31 11:12:09 +0530

    `Refactored file upload and analysis functionality, added save analysis feature and improved chart download functionality.`

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -42,6 +42,7 @@ export const loginUser = async (req, res) => {
         _id: user._id,
         name: user.name,
         email: user.email,
+        isAdmin: user.isAdmin,
         token: generateToken(user._id),
       });
     } else {
